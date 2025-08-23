bc189879351dd570f7cbff88fd0d839bbddf49b8
Scott Thomas <s@sthomas.id.au>
2025-08-23 12:58:05 +0930

    Remove hardcoded local testing environment config

--- docker/mysql/create-testing-database.sh
+++ docker/mysql/create-testing-database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS testing;
-    GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
+    CREATE DATABASE IF NOT EXISTS bushdiversva_phpunit;
+    GRANT ALL PRIVILEGES ON \`bushdiversva_phpunit%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
